c4c6954a9dfb46ba061f7493ebe08517b0c7997f
Austin Ellis <Austin@Janets-iMac.local>
2020-11-23 17:30:37 -0500

    added response.map from query selection of mysql tables

--- server.js
+++ server.js
@@ -19,17 +19,23 @@ connection.connect(err => {
     console.log(chalk.green(`Connected to database. ThreadID: ${connection.threadId}`));
 });
 
+let listDep;
+let listRoles;
+let listEmp;
 
 connection.query("SELECT * FROM roles", function(err, res) {
     if (err) throw err;
+    listRoles = res.map(role => ({ name: role.title, value: role.id }));
 });
 
 connection.query("SELECT * FROM departments", function(err, res) {
     if (err) throw err;
+    listDep = res.map(dep => ({ name: departFocus.name, value: dep.id }));
 });
 
 connection.query("SELECT * FROM employees", function(err, res) {
     if (err) throw err;
+    listEmp = res.map(emp => ({ name: `${emp.first_name}${emp.last_name}`, value: emp.id }));
 });
 
 start();
