cbb425a78f957e2cfa3c192309efa5ed226c2db7
Austin Ellis <Austin@Janets-iMac.local>
2020-11-23 17:25:37 -0500

    begin start function and added connection.query for the different tables

--- server.js
+++ server.js
@@ -2,3 +2,38 @@ const mysql = require("mysql");
 const inquirer = require("inquirer");
 const chalk = require("chalk");
 const cTable = require("console.table");
+
+const connection = mysql.createConnection({
+    host: "localhost",
+    PORT: 3000,
+    user: "root",
+    password: "Mickey19",
+    database: "employee_db"
+});
+
+connection.connect(err => {
+    if (err) {
+        console.log(chalk.white.bgRed(err));
+        return;
+    }
+    console.log(chalk.green(`Connected to database. ThreadID: ${connection.threadId}`));
+});
+
+
+connection.query("SELECT * FROM roles", function(err, res) {
+    if (err) throw err;
+});
+
+connection.query("SELECT * FROM departments", function(err, res) {
+    if (err) throw err;
+});
+
+connection.query("SELECT * FROM employees", function(err, res) {
+    if (err) throw err;
+});
+
+start();
+
+function start() {
+
+}
