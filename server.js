ccae21a47d150ac37dd905625f578f916acf2624
Austin Ellis <Austin@Janets-iMac.local>
2020-11-23 22:32:45 -0500

    added end function and add department function

--- server.js
+++ server.js
@@ -19,6 +19,21 @@ connection.connect(err => {
     console.log(chalk.green(`Connected to database. ThreadID: ${connection.threadId}`));
 });
 
+const startScreen = [
+    "View all Employees",
+    "View all Employees by Department",
+    "View all Departments",
+    "View all Roles",
+    "Add Employee",
+    "Add Department",
+    "Add Role",
+    "Update Employee Role",
+    "Remove Employee",
+    "Remove Department",
+    "Remove Role",
+    "Exit"
+];
+
 let listDep;
 let listRoles;
 let listEmp;
@@ -41,5 +56,82 @@ connection.query("SELECT * FROM employees", function(err, res) {
 start();
 
 function start() {
+    inquirer.prompt({
+        name: "action",
+        type: "list",
+        message: "What would you like to do?",
+        choices: [
+            "Add departments",
+            "View departments",
+            "Delete departments",
+            "Add roles",
+            "View roles",
+            "Delete roles",
+            "Add employees",
+            "View employees",
+            "Update employee roles",
+            "End"
+        ]
+    }).then(function(res) {
+        console.log(res);
+        switch (res.action) {
+            case "Add departments":
+                addDep();
+                break;
+            case "View departments":
+                viewDep();
+                break;
+            case "Delete departments":
+                delDep();
+                break;
+            case "Add roles":
+                addRole();
+                break;
+            case "View roles":
+                viewRole();
+                break;
+            case "Delete roles":
+                delRole();
+                break;
+            case "Add employees":
+                addEmp();
+                break;
+            case "View employees":
+                viewEmp();
+                break;
+            case "Delete employees":
+                delEmp();
+                break;
+            case "Update employee roles":
+                updateRole();
+                break;
+            case "End":
+                end();
+                break;
+        }
+    });
+}
 
+function addDep(data) {
+    inquirer.prompt([
+        {
+            type: "input",
+            message: "What is the name of the new department",
+            name: "name"
+        }
+    ]).then(function(res) {
+        connection.query("INSERT INTO departments SET ?", { name: res.name }, function(error, res) {
+            if (error) throw error;
+        });
+    }).then(function() {
+        console.log(`-----Department Added!-----`);
+    }).then(function() {
+        start();
+    });
+}
+
+function end() {
+    console.log("All done!");
+    connection.end();
+    process.exit();
 }
