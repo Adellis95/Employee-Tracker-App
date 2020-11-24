82210db16db4e13dadcc1a6c32d19f7eccdd3a22
Austin Ellis <Austin@Janets-iMac.local>
2020-11-24 02:33:31 -0500

    Attempt error handling

--- db/schema.sql
+++ db/schema.sql
@@ -4,17 +4,25 @@ CREATE DATABASE employee_db;
 
 USE employee_db;
 
-CREATE TABLE departments(
-	id INTEGER NOT NULL AUTO_INCREMENT,
+CREATE TABLE departments (id INTEGER NOT NULL AUTO_INCREMENT,
     department_name VARCHAR(45),
     PRIMARY KEY (id)
 );
 
-CREATE TABLE roles(
-	id INTEGER NOT NULL AUTO_INCREMENT,
+CREATE TABLE roles (id INTEGER NOT NULL AUTO_INCREMENT,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL(8, 2) NOT NULL,
     department_id INT NOT NULL,
     FOREIGN KEY (department_id) REFERENCES departments(id),
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE employees (id INTEGER NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INTEGER,
+    manager_id INTEGER,
+    FOREIGN KEY (role_id) REFERENCES roles (id),
+    FOREIGN KEY (manager_id) REFERENCES employees(id),
     PRIMARY KEY (id)
 );
